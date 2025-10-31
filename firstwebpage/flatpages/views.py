eabc4883cd37de92c855d42d0e805922b94f968c
alexmurashmail-creator <alexmurashmail@gmail.com>
2025-10-31 13:53:42 +0300

    Add files via upload

---
+++ firstwebpage/flatpages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django import template
+
+
+# Create your views here.
+#def home(request):
+#    return HttpResponse(u'Hello, world!', content_type="text/plain")
+def home(request):
+    return render(request, 'templates/index.html')
+def static_handler(request):
+    return render(request, 'templates/static_handler.html')
+def hello(request):
+    return HttpResponse(u'Hello world!')
